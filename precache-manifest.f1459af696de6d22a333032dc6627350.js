baf4e65039a5319194ef368556dd8bac06c4c7f1
dsijakovski98 <dsijakovski98@github.com>
2020-09-28 20:09:30 +0200

    Updates

---
+++ precache-manifest.f1459af696de6d22a333032dc6627350.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff7bfb0d3681e090cd24d42d2180731",
+    "url": "/popovi-winery/index.html"
+  },
+  {
+    "revision": "902b860c190497d8b3c8",
+    "url": "/popovi-winery/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "802546216cc7b2d694d7",
+    "url": "/popovi-winery/static/css/main.37bfb788.chunk.css"
+  },
+  {
+    "revision": "902b860c190497d8b3c8",
+    "url": "/popovi-winery/static/js/2.9d842f91.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/popovi-winery/static/js/2.9d842f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "802546216cc7b2d694d7",
+    "url": "/popovi-winery/static/js/main.00fa4894.chunk.js"
+  },
+  {
+    "revision": "cdd0281cd861a2f4bf2a",
+    "url": "/popovi-winery/static/js/runtime-main.f273686e.js"
+  },
+  {
+    "revision": "58745646bfbf275977327bf9296f58a7",
+    "url": "/popovi-winery/static/media/awards2.58745646.png"
+  },
+  {
+    "revision": "1622287f4066c79050b61112fafaadf7",
+    "url": "/popovi-winery/static/media/awards3.1622287f.jpg"
+  },
+  {
+    "revision": "3d82158032199ad066ed31ffdc93de90",
+    "url": "/popovi-winery/static/media/cabernet.3d821580.jpg"
+  },
+  {
+    "revision": "62c0eb20bcea8ec9967891aeae23dd26",
+    "url": "/popovi-winery/static/media/fifth.62c0eb20.jpg"
+  },
+  {
+    "revision": "9ff5f875e857cd2fbe2a386bfd14ca71",
+    "url": "/popovi-winery/static/media/first.9ff5f875.jpg"
+  },
+  {
+    "revision": "33534b63b87d6733f9b62245aed1b525",
+    "url": "/popovi-winery/static/media/fourth.33534b63.jpg"
+  },
+  {
+    "revision": "15556c6f3105520c5c9555cef7e82361",
+    "url": "/popovi-winery/static/media/header-bg-2.15556c6f.jpg"
+  },
+  {
+    "revision": "ffb9af4e286bc34307457e801e0344c6",
+    "url": "/popovi-winery/static/media/header-bg.ffb9af4e.jpg"
+  },
+  {
+    "revision": "155f3ba1b2aa9e3e8b6ca0720deea149",
+    "url": "/popovi-winery/static/media/logo-light.155f3ba1.png"
+  },
+  {
+    "revision": "a4f141742593f118ae4838291c9ef596",
+    "url": "/popovi-winery/static/media/logo.a4f14174.png"
+  },
+  {
+    "revision": "0da25a0566489d4ea7dc9645f7090349",
+    "url": "/popovi-winery/static/media/saem.0da25a05.jpg"
+  },
+  {
+    "revision": "51f51c5c616cf0964df9e5fcf9c9f344",
+    "url": "/popovi-winery/static/media/second.51f51c5c.jpg"
+  },
+  {
+    "revision": "3fbb8d832720d032dbca7a54ae814358",
+    "url": "/popovi-winery/static/media/temjanika.3fbb8d83.jpg"
+  },
+  {
+    "revision": "a58623c13895ead530886c568b1041db",
+    "url": "/popovi-winery/static/media/third.a58623c1.jpg"
+  },
+  {
+    "revision": "5dce4eb2a2cf07aa56e3510ef6c708ca",
+    "url": "/popovi-winery/static/media/vranec1.5dce4eb2.jpg"
+  },
+  {
+    "revision": "0cb768439f52c8c37563afe62e5f967e",
+    "url": "/popovi-winery/static/media/vranec2.0cb76843.jpg"
+  }
+]);
